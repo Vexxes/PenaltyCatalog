4ad4a8e9e57eab0f065ca7257ff0eba723f8ff88
Vexxes <schniicks@hotmail.de>
2022-11-06 23:17:53 +0100

    Initial commit + created BottomBar Navigation

---
+++ app/src/main/java/de/vexxes/penaltycatalog/ui/theme/Theme.kt
@@ -0,0 +1,95 @@
+package de.vexxes.penaltycatalog.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Blue80,
+    onPrimary = Blue20,
+    primaryContainer = Blue30,
+    onPrimaryContainer = Blue90,
+    inversePrimary = Blue40,
+    secondary = GreyBlue80,
+    onSecondary = GreyBlue20,
+    secondaryContainer = GreyBlue30,
+    onSecondaryContainer = GreyBlue90,
+    tertiary = Purple80,
+    onTertiary = Purple20,
+    tertiaryContainer = Purple30,
+    onTertiaryContainer = Purple90,
+    background = Neutral10,
+    onBackground = Neutral90,
+    surface = Neutral10,
+    onSurface = Neutral90,
+    surfaceVariant = NeutralVariant30,
+    onSurfaceVariant = NeutralVariant80,
+    inverseSurface = Neutral90,
+    inverseOnSurface = Neutral20,
+    error = Error80,
+    onError = Error20,
+    errorContainer = Error30,
+    onErrorContainer = Error90,
+    outline = NeutralVariant60,
+)
+
+val LightColorScheme = lightColorScheme(
+    primary = Blue40,
+    onPrimary = Blue100,
+    primaryContainer = Blue90,
+    onPrimaryContainer = Blue10,
+    inversePrimary = Blue80,
+    secondary = GreyBlue40,
+    onSecondary = GreyBlue100,
+    secondaryContainer = GreyBlue90,
+    onSecondaryContainer = GreyBlue10,
+    tertiary = Purple40,
+    onTertiary = Purple100,
+    tertiaryContainer = Purple90,
+    onTertiaryContainer = Purple10,
+    background = Neutral99,
+    onBackground = Neutral10,
+    surface = Neutral99,
+    onSurface = Neutral10,
+    surfaceVariant = NeutralVariant90,
+    onSurfaceVariant = NeutralVariant30,
+    inverseSurface = Neutral20,
+    inverseOnSurface = Neutral95,
+    error = Error40,
+    onError = Error100,
+    errorContainer = Error90,
+    onErrorContainer = Error10,
+    outline = NeutralVariant50,
+)
+
+@Composable
+fun PenaltyCatalogTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+/*    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
+    }*/
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
