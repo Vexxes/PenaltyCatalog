13dfcb3735f2cbd77329f7f475d0251ffdecf6a1
Vexxes <94523525+Vexxes@users.noreply.github.com>
2023-01-16 23:16:22 +0100

    Reworked PenaltyType screens and changed rest calls for PenaltyType

---
+++ app/src/main/java/de/vexxes/penaltycatalog/domain/visualTransformation/NumberCommaTransformation.kt
@@ -0,0 +1,29 @@
+package de.vexxes.penaltycatalog.domain.visualTransformation
+
+import android.icu.text.NumberFormat
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
+import java.util.Locale
+
+// format long to 123,456,789,9
+class NumberCommaTransformation : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        return TransformedText(
+            text = AnnotatedString(text.text.toLongOrNull().formatWithComma()),
+            offsetMapping = object : OffsetMapping {
+                override fun originalToTransformed(offset: Int): Int {
+                    return text.text.toLongOrNull().formatWithComma().length
+                }
+
+                override fun transformedToOriginal(offset: Int): Int {
+                    return text.length
+                }
+            }
+        )
+    }
+}
+
+fun Long?.formatWithComma(): String =
+    NumberFormat.getNumberInstance(Locale.US).format(this ?: 0)
